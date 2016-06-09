2a382b0465c968023f099405e15aebe692d92472
Even Østvold <eos@difi.no>
2016-06-09 15:37:41 +0200

    changed logging to better describe error scenarios

--- brukar_client/brukar_client.oauth.php
+++ brukar_client/brukar_client.oauth.php
@@ -1,20 +1,17 @@
 <?php
-
 /**
  * @file
  */
-
 function brukar_client_oauth_request() {
   require_once(drupal_get_path('module', 'brukar_common') . '/OAuth.php');
-
   $method = new OAuthSignatureMethod_HMAC_SHA1();
   $consumer = new OAuthConsumer(variable_get('brukar_consumer_key'), variable_get('brukar_consumer_secret'));
 
   // Better solution available?
   $query = $_GET;
   unset($query['q']);
-  $callback =  url($_GET['q'], array('absolute' => TRUE, 'query' => $query));
 
+  $callback =  url($_GET['q'], array('absolute' => TRUE, 'query' => $query));
   $req = OAuthRequest::from_consumer_and_token($consumer, NULL, 'GET', variable_get('brukar_url') . 'server/oauth/request_token', array('oauth_callback' => $callback));
   $req->sign_request($method, $consumer, NULL);
   parse_str(trim(file_get_contents($req->to_url())), $token);
@@ -24,15 +21,17 @@ function brukar_client_oauth_request() {
     drupal_goto(variable_get('brukar_url') . 'server/oauth/authorize?oauth_token=' . $token['oauth_token']);
   }
   else {
+    // Unable to retrive Rrequest token
     $debug_data = array(
-        'request_uri' => request_uri(),
-        'auth_oauth' =>  isset($_SESSION['auth_oauth']) ? $_SESSION['auth_oauth'] : 'no auth_oauth');
+      'request_uri' => request_uri(),
+      'Request token http_response_header' => $http_response_header,
+      'session' =>  $_SESSION,
+      'token' => $token);
     watchdog(
-        'brukar_client',
-        'Unable to retrieve token for login.<br/>Debug data:<br/><pre>!debug_data</pre><br/>',
-        array('!debug_data' =>  print_r($debug_data, TRUE) ),
-        WATCHDOG_ERROR);
-
+      'brukar_client',
+      'Unable to retrieve reqest token for login.<br/>Debug data:<br/><pre>!debug_data</pre><br/>',
+      array('!debug_data' =>  print_r($debug_data, TRUE) ),
+      WATCHDOG_ERROR);
     drupal_set_message(t('Unable to retrieve token for login.'), 'warning');
     drupal_goto('<front>');
   }
@@ -40,41 +39,54 @@ function brukar_client_oauth_request() {
 
 function brukar_client_oauth_callback() {
   require_once(drupal_get_path('module', 'brukar_common') . '/OAuth.php');
-
   $method = new OAuthSignatureMethod_HMAC_SHA1();
   $consumer = new OAuthConsumer(variable_get('brukar_consumer_key'), variable_get('brukar_consumer_secret'));
 
   if (isset($_SESSION['auth_oauth']) && $_SESSION['auth_oauth']['oauth_token'] == $_GET['oauth_token']) {
     unset($_GET['oauth_token']);
-    $tmp = new OAuthToken($_SESSION['auth_oauth']['oauth_token'], $_SESSION['auth_oauth']['oauth_token_secret']);
 
+    $tmp = new OAuthToken($_SESSION['auth_oauth']['oauth_token'], $_SESSION['auth_oauth']['oauth_token_secret']);
     $req = OAuthRequest::from_consumer_and_token($consumer, $tmp, 'GET', variable_get('brukar_url') . 'server/oauth/access_token', array());
     $req->sign_request($method, $consumer, $tmp);
     parse_str(trim(file_get_contents($req->to_url())), $token);
-
+    $orig_sesion_token = $_SESSION['auth_oauth'];
     unset($_SESSION['auth_oauth']);
 
     if (count($token) > 0) {
       $_SESSION['_brukar_access_token'] = array('token' => $token['oauth_token'], 'token_secret' => $token['oauth_token_secret']);
       $token = new OAuthToken($token['oauth_token'], $token['oauth_token_secret']);
-
       $req = OAuthRequest::from_consumer_and_token($consumer, $token, 'GET', variable_get('brukar_url') . 'server/oauth/user', array());
       $req->sign_request($method, $consumer, $token);
-
       brukar_client_login((array) json_decode(trim(file_get_contents($req->to_url()))));
+    }else{
+      // Unable to retrive Access token
+      $debug_data = array(
+        'token' => $token,
+        'request_uri' => request_uri(),
+        'access token http_response_header' => $http_response_header,
+        'session' =>  $_SESSION,
+        'session_oauth' => $orig_sesion_token);
+      watchdog(
+        'brukar_client',
+        'Access token error.<br/>Debug data:<br/><pre>!debug_data</pre><br/>',
+        array( '!debug_data' =>  print_r($debug_data, TRUE) ),
+        WATCHDOG_ERROR
+      );
     }
-  }
-
-  $debug_data = array(
+  }else{
+    // Request token mismatch
+    $debug_data = array(
       'cookie' => $_COOKIE,
       'request_uri' => request_uri(),
-      'auth_oauth' =>  isset($_SESSION['auth_oauth']) ? $_SESSION['auth_oauth'] : 'no auth_oauth');
-  watchdog(
+      'session' =>  $_SESSION);
+    watchdog(
       'brukar_client',
-      'User login failed.<br/>Debug data:<br/><pre>!debug_data</pre><br/>',
-      array('!debug_data' =>  print_r($debug_data, TRUE) ),
+      'Request token error.<br/>Debug data:<br/><pre>!debug_data</pre><br/>',
+      array( '!debug_data' =>  print_r($debug_data, TRUE) ),
       WATCHDOG_ERROR);
+  }
 
+  watchdog('brukar_client', 'Noe gikk feil under innlogging.', array(), WATCHDOG_ERROR);
   drupal_set_message(t('Noe gikk feil under innlogging.'), 'warning');
   drupal_goto('<front>');
 }
@@ -96,14 +108,16 @@ function brukar_client_login($data) {
   if ($user->uid != 0) {
     user_save($user, $edit);
     user_set_authmaps($user, array('authname_brukar' => $data['id']));
-      drupal_goto('user');
+    watchdog('brukar_client', 'User already loged in, redirecting to front.', array(), WATCHDOG_DEBUG);
+    drupal_goto('user');
   }
 
   $authmap_user = db_query('SELECT uid FROM {authmap} WHERE module = :module AND authname = :ident', array(':ident' => $data['id'], ':module' => 'brukar'))->fetch();
   if ($authmap_user === FALSE) {
     $provided = module_invoke_all('brukar_client_user', $edit);
-	$user = !empty($provided) ? $provided[0] : user_save(user_load_by_name($data['id']), $edit);
+    $user = !empty($provided) ? $provided[0] : user_save(user_load_by_name($data['id']), $edit);
     user_set_authmaps($user, array('authname_brukar' => $data['id']));
+    watchdog('brukar_client', 'User authmap mapping created:<nr/><pre>!debug_data</pre><br/>', array('!debug_data' => print_r(array('user_id' => $user->uid, 'brukar_ident' => $data['id']), TRUE)), WATCHDOG_INFO);
   }
   else {
     $user = user_save(user_load($authmap_user->uid), $edit);
