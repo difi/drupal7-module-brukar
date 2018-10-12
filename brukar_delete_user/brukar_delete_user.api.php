2c600e9ca1f970878487b49b0acf45c2d2b726cc
Håvard Tørresen <hto@difi.no>
2018-10-12 10:00:27 +0200

    TLP-114 hook i sletteprosessen

---
+++ brukar_delete_user/brukar_delete_user.api.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Called when receiving delete-message from Brukar,
+ * after $user->field_delete has been set.
+ *
+ * Only called if user exists
+ */
+function hook_brukar_delete_user_incoming_delete($uid){
+
+}
